04e4b765b9ca024bc076bfd0050c186ab9d02340
Emily S <emily.sugano@gmail.com>
2018-07-12 22:03:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,8 +14,7 @@ class App < Sinatra::Base
   end
 
   get '/goodbye/:name' do
-    @name = params[:name]
-    "Goodbye #{@name}"
+    "Goodbye #{params[:name]}"
   end
 
   get '/multiply/:num1/:num2' do
