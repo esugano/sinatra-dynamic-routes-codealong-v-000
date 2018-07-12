8d520a6164a4733c377dcf303c43c34047154cee
Emily S <emily.sugano@gmail.com>
2018-07-12 21:55:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,12 +14,12 @@ class App < Sinatra::Base
   end
 
   get '/goodbye/:name' do
-    @name = params[:id]
+    @name = params[:name]
     "Goodybye #{@name}"
   end
 
   get '/multiply/:num1/:num2' do
-    @num_1 = params[:id]
+    @num_1 = params[:id] 
     # @num_2 = params[:id][1]
     # @product = @num1.to_i * @num_2.to_i
     "Hello #{@num_1}"
